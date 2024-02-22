a9cdc80bf06e2815fadcd2f5be6f4f290986c4ce
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2024-02-22 19:31:31 +0100

    Update README.md with user and developer instructions

---
+++ GudModeFix.cs
@@ -0,0 +1,133 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+
+namespace gudModeFix;
+public class HelloWorldPlugin : BasePlugin
+{
+    public override string ModuleName => "GudMode Fix";
+    public override string ModuleVersion => "0.1.1";
+    private bool _is_round_started = false;
+    private List<CCSPlayerController> _players = new();
+
+    public override void Load(bool hotReload) {
+        Log.console(ConsoleColor.Green, $"Loading '{ModuleName}' version {ModuleVersion}!");
+    }
+
+    public override void Unload(bool hotReload) {
+        Log.console(ConsoleColor.Green, $"Unloading '{ModuleName}' version {ModuleVersion}.");
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info) {
+        _is_round_started = true;
+
+        _players = Utilities.GetPlayers();
+
+        Log.console(ConsoleColor.Green, $"Round started with {@event.Timelimit}, checking {_players.Count} players.");
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info) {
+        _is_round_started = false;
+
+        _players = new();
+
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler(HookMode.Pre)]
+    public HookResult OnPlayerChangeTeam(EventPlayerTeam @event, GameEventInfo info) {
+        // If round is not started we dont care for team changes
+        if (!_is_round_started) 
+            return HookResult.Continue;
+
+        // Filter out new players, players goint to spectator and players that are not changing teams
+        /**
+         * 0 - Unassigned
+         * 1 - Spectator
+         * 2 - Terror
+         * 3 - Counter-Terror
+        */
+        if (
+            @event.Team == 0 ||
+            @event.Team == 1 ||
+            @event.Oldteam == 0 ||
+            @event.Team == @event.Oldteam
+            ) 
+            return HookResult.Continue;
+
+        CCSPlayerController player = @event.Userid;
+
+        // Check if player was when the round started
+        if(_players.Find(List => List == player) == null)
+            return HookResult.Continue;
+
+        // Filter out invalid players
+        if (
+            player is null || 
+            !player.IsValid || 
+            !player.PlayerPawn.IsValid
+            )
+            return HookResult.Continue;
+
+        Log.console(ConsoleColor.Yellow, $"Player {@event.Userid.PlayerName} tried to change teams from {@event.Oldteam} to {@event.Team}, killing to prevent godmode glich.");
+
+        /*
+         * Atempt to kill the player, but didnt stop the godmode glitch as the player seems to be in a stare where he is not alive but spawmed
+            CCSPlayerPawn pawn = player.PlayerPawn.Value;
+            pawn.CommitSuicide(true, true);
+        */
+         
+        // Setting this as handled will prevent the player from changing teams
+        return HookResult.Handled;
+    }
+}
+
+public static class Log
+{
+    private static string ModulePrefix = "GudMode Fix";
+
+    public static void consoleLog(string message)
+    {
+        Console.WriteLine($"[{ModulePrefix}] " + message);
+    }
+
+    public static void console(ConsoleColor color, string message)
+    {
+        Console.ForegroundColor = color;
+        consoleLog(message); 
+        Console.ResetColor();
+    }
+
+    public static void chat(string message) {
+        Server.PrintToChatAll(message.chatColorer());
+    }
+
+    private static string chatColorer(this string message) {
+        message = message.Replace("{DEFAULT}", $"{ChatColors.Default}");
+        message = message.Replace("{WHITE}", $"{ChatColors.White}");
+        message = message.Replace("{DARKRED}", $"{ChatColors.Darkred}");
+        message = message.Replace("{GREEN}", $"{ChatColors.Green}");
+        message = message.Replace("{LIGHTYELLOW}", $"{ChatColors.LightYellow}");
+        message = message.Replace("{LIGHTBLUE}", $"{ChatColors.LightBlue}");
+        message = message.Replace("{OLIVE}", $"{ChatColors.Olive}");
+        message = message.Replace("{LIME}", $"{ChatColors.Lime}");
+        message = message.Replace("{RED}", $"{ChatColors.Red}");
+        message = message.Replace("{PURPLE}", $"{ChatColors.Purple}");
+        message = message.Replace("{GREY}", $"{ChatColors.Grey}");
+        message = message.Replace("{YELLOW}", $"{ChatColors.Yellow}");
+        message = message.Replace("{GOLD}", $"{ChatColors.Gold}");
+        message = message.Replace("{SILVER}", $"{ChatColors.Silver}");
+        message = message.Replace("{BLUE}", $"{ChatColors.Blue}");
+        message = message.Replace("{DARKBLUE}", $"{ChatColors.DarkBlue}");
+        message = message.Replace("{BLUEGREY}", $"{ChatColors.BlueGrey}");
+        message = message.Replace("{MAGENTA}", $"{ChatColors.Magenta}");
+        message = message.Replace("{LIGHTRED}", $"{ChatColors.LightRed}");
+
+        return message;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# GudModeFix
+# 🪁 GudModeFix
 This plugin attemps to fix the new glich that was discovered in cs2 to become invencible by changing teams in game startup
+
+## 🧶 User Instuctions
+1. Go to the [**releases**](https://github.com/IMXNOOBX/GudModeFix/releases) page and download the latest version
+
+2. Extract the zip file to a folder of your choice
+
+3. Go to your cs2 server folder *`/game/csgo/addons/counterstrikesharp/plugins`*
+
+4. Create a folder with the name *`GudModeFix`* and put the *`GudModeFix.dll`* and the rest of the files in the zip inside
+
+5. **Restart** the cs2 server and the plugin should be loaded and display a message in the console after a successfull load
+
+* ❕For more information follow [**this guide**](https://docs.cssharp.dev/docs/guides/getting-started.html)
+
+## 📘 Developer Instructions
+
+1. Build the program using **Visual Studio 2022**
+	- Build: **`Build > Build Current Document`**
+
+2. Locate your binary file in the folder `bin\Debug\net7.0\GudModeFix.dll`
+* ❕For more information follow [**this guide**](https://docs.cssharp.dev/docs/guides/hello-world-plugin.html)
+
+
+# 🔖 License & Copyright
+
+This project is licensed under [**CC BY-NC 4.0**](https://creativecommons.org/licenses/by-nc/4.0/).
+```diff
++ You are free to:
+	• Share: Copy and redistribute the material in any medium or format.
+	• Adapt: Remix, transform, and build upon the material.
++ Under the following terms:
+	• Attribution: You must give appropriate credit, provide a link to the original source repository, and indicate if changes were made.
+	• Non-Commercial: You may not use the material for commercial purposes.
+- You are not allowed to:
+	• Sell: This license forbids selling original or modified material for commercial purposes.
+	• Sublicense: This license forbids sublicensing original or modified material.
+```
+### ©️ Copyright
+The content of this project is ©️ by [IMXNOOBX](https://github.com/IMXNOOBX) and the respective contributors. See the [LICENSE.md](LICENSE.md) file for details.
